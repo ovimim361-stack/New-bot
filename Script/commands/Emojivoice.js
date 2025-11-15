0381e376f978b5873eabb8f016c6f90675db1eb1
ovimim361-stack <ovimim361@gmail.com>
2025-11-15 10:39:01 +0600

    Update Emojivoice.js

--- Script/commands/Emojivoice.js
+++ Script/commands/Emojivoice.js
@@ -44,7 +44,7 @@ const emojiAudioMap = {
  "🥹": "https://files.catbox.moe/jf85xe.mp3",
  "😩": "https://files.catbox.moe/b4m5aj.mp3",
  "🫣": "https://files.catbox.moe/ttb6hi.mp3",
- "🤧": "https://files.catbox.moe/pfe8n4.mp3"‚
+ "🤧": "https://files.catbox.moe/pfe8n4.mp3",
  "🐸": "https://files.catbox.moe/utl83s.mp3"
 };
 

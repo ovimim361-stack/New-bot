19d5f07e6cda33e57063fa0ea2e054852f73b29e
ovimim361-stack <ovimim361@gmail.com>
2025-12-02 09:29:09 +0600

    Update unsend.js

--- Script/commands/unsend.js
+++ Script/commands/unsend.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-	name: "delet",
+	name: "unsend",
 	version: "1.0.1",
 	hasPermssion: 0,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Gỡ tin nhắn của bot",
 	commandCategory: "system",
-	usages: "delet",
+	usages: "unsend",
 	cooldowns: 0
 };
 
